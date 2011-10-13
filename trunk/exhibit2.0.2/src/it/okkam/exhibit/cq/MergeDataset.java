f7bd8f5d0260021ce3b2df1d51da88b9f40ab987
seidymam <seidymam@70d5fa3b-c89e-f69a-9c68-ad4c4b28401d>
2011-10-13 09:00:42 +0000

    CQ

--- trunk/exhibit2.0.2/src/it/okkam/exhibit/cq/MergeDataset.java
+++ trunk/exhibit2.0.2/src/it/okkam/exhibit/cq/MergeDataset.java
@@ -1,25 +1,15 @@
 package it.okkam.exhibit.cq;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
 /*
  * CQ-001: We want to get information about locations from two different triple store, merge those
  * sub graphs and show the result into an exhibit.
  */
 public class MergeDataset {
 	
-	//static String localService = "http://localhost:2020/rivela";
+	static String localService = "http://localhost:2020/rivela";
 	static String remoteService = "http://192.168.234.87:2020/taxproject";
 	
 	String queryString = " prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
@@ -28,30 +18,19 @@ public class MergeDataset {
 		+ "prefix dc: <http://purl.org/dc/elements/1.1/>"
 		+ "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
 		+ "prefix ens: <http://models.okkam.org/ENS-meta-core-vocabulary.owl#> "
-		+ "select distinct ?subjectLocation where { graph ?g { "
-		+ "?subjectLocation a ens:location ."		
+		+ "Construct { ?s ?p ?o   } where { graph ?g { "
+		+ "?s ?p ?o ."		
 		+ " } }";
 	
 	public void callRemoteService() {
 		
 		
-		QueryExecution queryObject = QueryExecutionFactory.sparqlService(
-				remoteService, queryString);		
-		
-		try {
-			ResultSet results = queryObject.execSelect();
-			ArrayList resultLists = new ArrayList();
-			while (results.hasNext()) {
-				QuerySolution solution = results.nextSolution();
-				// System.out.println(solution);
-				String location = solution.get("?subjectLocation").toString();
-								
-				System.out.println("Location: " + location);
-			}			
-		} finally {
-			queryObject.close();
-		}
-		
+		QueryExecution queryObject = QueryExecutionFactory.sparqlService(remoteService, queryString);		
+		Model remote= queryObject.execConstruct();
+		queryObject = QueryExecutionFactory.sparqlService(localService, queryString);
+		Model local=queryObject.execConstruct();
+		remote.add(local);
+		remote.write(System.out,"TTL");
 	}
 
 }
